4ea94be75622e0c280a569d2ca320291fe7abd67
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-02-27 17:08:40 -0800

    cleaned code

--- Easy/015_3Sum/test_015.py
+++ Easy/015_3Sum/test_015.py
@@ -10,9 +10,9 @@ class WidgetTestCase(unittest.TestCase):
         nums = [-1, 0, 1, 2, -1, -4]
         expected = [[-1, 0, 1], [-1, -1, 2]]
         output = self.sol.threeSum(nums)
-        expect_sorted = sorted(expected, key=lambda x: str(x[0]) + "," + str(x[1]) + "," + str(x[2]))
-        output_sorted = sorted(output, key=lambda x: str(x[0]) + "," + str(x[1]) + "," + str(x[2]))
-        self.assertEqual(output_sorted, expect_sorted)
+        expect_sorted = sorted(expected, key=lambda x: (x[0], x[1], x[2]))
+        output_sorted = sorted(output, key=lambda x: (x[0], x[1], x[2]))
+        self.assertEqual(output_sorted, expect_sorted, 'Incorrect lists result')
 
     def tearDown(self):
         del self.sol
